d47afef75fb65b1f387b5a8f0a7dbaf9c96583b6
idamachmadfaizin <idamachmadfaizin@gmail.com>
2019-01-17 13:56:00 +0700

    add assets

---
+++ application/views/partials/footer.php
@@ -0,0 +1,75 @@
+
+<footer class="bg-gray">
+   <div class="container">
+      <div class="row" style="padding-bottom: 50px;">
+         <div class="col-sm-5 row-footer">
+            <h6><b>GET IN TOUCH</b></h6>
+            <br>
+            <p>Lorem Ipsum is simply dummy text of the printing <br>and typesetting industry.</p>
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-instagram" style="margin-left:20px;"></i>
+            <i class="fab fa-twitter" style="margin-left:20px;"></i>
+         </div>
+         <div class="col-sm-2 row-footer">
+            <h6><b>MY ACCOUNT</b></h6>
+            <br>
+            <p><a href="#" class="nounderline">My Account</a></p>
+            <p><a href="#" class="nounderline">Order History</a></p>
+            <p><a href="#" class="nounderline">Wishlist</a></p>
+         </div>
+         <div class="col-sm-2 row-footer">
+            <h6><b>PAYMENT</b></h6>
+            <br>
+            <img src="<?php echo base_url();?>/assets/img/logos/Logo-BCA.png" alt="">
+            <br>
+            <br>
+            <img src="<?php echo base_url();?>/assets/img/logos/Logo-JNE.png" alt="">
+         </div>
+         <div class="col-sm-3 row-footer">
+            <h6><b>CONTACT</b></h6>
+            <br>
+            <ul class="list-unstyled">
+               <li style="margin-bottom: 10px;">
+                  <div class="d-flex justify-content-center contact-box-icon">
+                     <i class="fas fa-map-marker-alt contact"></i>
+                  </div>
+                  <span>Swan Regency - Sidoarjo</span>
+               </li>
+               <li style="margin-bottom: 10px;">
+                  <div class="d-flex justify-content-center contact-box-icon">
+                     <i class="fas fa-phone contact"></i>
+                  </div>
+                  <span>082123456789</span>
+               </li>
+               <li style="margin-bottom: 10px;">
+                  <div class="d-flex justify-content-center contact-box-icon">
+                     <i class="fas fa-envelope contact"></i>
+                  </div>
+                  <span>Rumseafood@gmail.com</span>
+               </li>
+               <li style="margin-bottom: 10px;">
+                  <div class="d-flex justify-content-center contact-box-icon">
+                     <i class="fas fa-info contact"></i>
+                  </div>
+                  <span>About Us</span>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </div>
+</footer>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ application/views/partials/header.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+   <!-- My CSS -->
+   <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
+
+   <!-- Font Awesome -->
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+<!--==========================================================================================-->
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/vendor/bootstrap/css/bootstrap.min.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/fonts/themify/themify-icons.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/fonts/elegant-font/html-css/style.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/vendor/animate/animate.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/vendor/css-hamburgers/hamburgers.min.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/vendor/animsition/css/animsition.min.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/vendor/select2/select2.min.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/vendor/slick/slick.css">
+<!--==========================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/css/util.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fashe/css/main.css">
+<!--==========================================================================================-->
+
+   <title>Rum Seafood</title>
+</head>
+<body>
+
+<div class="container">
+
+   <div class="row d-flex justify-content-end">
+      <div class="col-4 group-header-icons d-flex justify-content-end">
+         <span class="header-icons">
+            <form class="form-inline my-2 my-lg-0" style="float:left;">
+               <input class="form-control form-control-sm mr-sm-4" type="search" placeholder="Search" aria-label="Search">
+            </form>
+            <a href="#"><img src="<?php echo base_url()?>assets/img/icons/ic_cart.png" alt="" class="mr-sm-3"></a>
+            <a href="#"><img class="header-icons" src="<?php echo base_url()?>assets/img/icons/ic_person.png" alt="icon person"></a>
+         </span>
+      </div>
+   </div>
+
+   <div class="row">
+      <div class="col mx-auto">
+         <img class="logorum mx-auto d-block" src="<?php echo base_url()?>assets/img/logos/logo_Rumseafood.png" alt="Logo Rumseafood">
+      </div>
+   </div>
+   
+   <!-- Navigation -->
+   <div class="row align-content-center">
+      <div class="col">
+         <nav class="navbar navbar-expand navbar-light bg-none rounded">
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars1" aria-controls="navbars1" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+            </button>
+         
+            <div class="collapse navbar-collapse justify-content-md-center" id="navbars1">
+               <ul class="navbar-nav">
+                  <li class="nav-item active">
+                     <a class="nav-link" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Products</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">About</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Contact</a>
+                  </li>
+               </ul>
+            </div>
+
+         </nav>
+
+      </div>
+   </div>
+
+</div>
