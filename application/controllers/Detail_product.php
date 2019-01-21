b243d44b31616746cdd1bec75655ac32a65f1028
idamachmadfaizin <idamachmadfaizin@gmail.com>
2019-01-21 21:42:56 +0700

    get detail produk

--- application/controllers/Detail_product.php
+++ application/controllers/Detail_product.php
@@ -3,8 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Detail_product extends CI_Controller {
 
-	public function index()
+	public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('Rum_model');   
+    }
+
+	public function detail($id_produk)
 	{
-        $this->load->view('detail_product_v');
+        $query = $this->Rum_model->getDetailProduk($id_produk);
+        $data['detailProduk'] = $query->result_array();
+
+        $this->load->view('detail_product', $data);
 	}
 }
