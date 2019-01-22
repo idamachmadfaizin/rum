5641305510687a9f389bd9d2b060e8196f259671
idamachmadfaizin <idamachmadfaizin@gmail.com>
2019-01-22 23:10:09 +0700

    delete func arrKmeans

--- application/models/Rum_model.php
+++ application/models/Rum_model.php
@@ -11,12 +11,7 @@ class Rum_model extends CI_Model
     {
         return $this->db->count_all('k_means');
     }
-    public function arrKmeans()
-    {
-        // $query = "SELECT n1, n2 FROM (SELECT id_detail_kmeans AS idk, nilai AS n1 FROM detail_kmeans WHERE id_detail_kmeans %2 <> 0) AS nilai1 JOIN (SELECT (id_detail_kmeans-1) AS idGanjil, nilai AS n2 FROM detail_kmeans WHERE id_detail_kmeans %2 = 0) AS nilai2 ON nilai1.idk = nilai2.idGanjil";
-        
-        return $this->db->query($query);
-    }
+
     public function getDetailProduk($id_produk)
     {
         $this->db->select('produk.id_produk, nama_kategori, nama_produk, harga_produk, deskripsi_produk, url_image');
