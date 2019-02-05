0f6eda05bbb9f7688af97e528e1018ae7551d4e1
idamachmadfaizin <idamachmadfaizin@gmail.com>
2019-02-05 16:31:42 +0700

    fix layout

--- application/views/login.php
+++ application/views/login.php
@@ -1,95 +1,90 @@
 <?php $this->load->view('partials/header2'); ?>
-
-  <div class="container" style="margin-top: 220px;">
+<div class="d-flex align-items-center" style="margin-top:80px; height:498px;">
+  <div class="container-fluid">
     <div class="row">
-      <div class="col-3">
-      </div>
-
+      <div class="col-3"></div>
 
       <!-- start -->
       <div class="col-lg-6">
-        <form action="#" method="post" class="">
+        <?= form_open() ?>
           <div class="form-group">
-            <div class="container-fluid">
-              <div class="row no-gutters">
-                <div class="col-1">
-                </div>
-                <div class="col-11">
-                  <span class="font-weight-bold">Login with email</span>
-                </div>
+            <div class="row">
+              <div class="col-1">
+              </div>
+              <div class="col-11">
+                <?php if(validation_errors()): ?>
+                  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <?= validation_errors(); ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                <?php endif; ?>
+
+                <?php if ($this->session->flashdata('error_session')):?>
+                  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <?= $this->session->flashdata('error_session'); ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                <?php endif; ?>
+
+                <span class="font-weight-bold">Login with email</span>
               </div>
             </div>
           </div>
 
           <div class="form-group">
-            <div class="container-fluid">
-              <div class="row no-gutters">
-                <!-- <div class="input-group"> -->
-                  <div class="col-1 no-gutters">
-                    <span style="padding: 11px 0px;" class="d-flex justify-content-center">
-                    <i class="fa fa-envelope"></i>
-                    </span>  
-                  </div>
-                  <div class="col-11">
-                    <input type="email" name="email" placeholder="Email" class="form-control">
-                  </div>
-                <!-- </div> -->
+            <div class="row">
+              <div class="col-1">
+                <span style="padding: 11px 0px;" class="d-flex justify-content-center">
+                  <i class="fa fa-envelope"></i>
+                </span>  
               </div>
-            </div>
-          </div>
-          
-          <div class="form-group">
-            <div class="container-fluid">
-              <div class="row no-gutters">
-                <!-- <div class="input-group"> -->
-                  <div class="col-1 no-gutters">
-                    <span style="padding: 11px 0px;" class="d-flex justify-content-center">
-                      <i class="fas fa-lock"></i>
-                    </span>  
-                  </div>
-                  <div class="col-11">
-                    <input type="password" name="pass" placeholder="Password" class="form-control">
-                  </div>
-                <!-- </div> -->
+              <div class="col-11">
+                <input type="email" name="email" value="<?= set_value('email');?>" placeholder="Email" class="form-control">
               </div>
             </div>
           </div>
 
           <div class="form-group">
-            <div class="container-fluid">
-              <div class="row no-gutters">
-                <!-- <div class="input-group"> -->
-                  <div class="col-1 no-gutters"> 
-                  </div>
-                  <div class="col-11">
-                    <button type="submit" class="btn btn-danger btn-block">Login</button>
-                  </div>
-                <!-- </div> -->
+            <div class="row">
+              <div class="col-1">
+                <span style="padding: 11px 0px;" class="d-flex justify-content-center">
+                  <i class="fas fa-lock"></i>
+                </span>
+              </div>
+              <div class="col-11">
+                <input type="password" name="password" placeholder="Password" class="form-control">
               </div>
             </div>
           </div>
-        </form>
 
-      <div class="col-3">
+          <div class="form-group">
+            <div class="row">
+              <div class="col-1"></div>
+              <div class="col-11">
+                <button type="submit" class="btn btn-danger btn-block">Login</button>
+              </div>
+            </div>
+          </div>
+        <?= form_close(); ?>
       </div>
-    </div>
-  </div>
+      <div class="col-3"></div>
+    </div> <!-- end row -->
+  </div> <!-- End container -->
 </div>
 
-<div class="container-fluid" style="margin-top: 153px; padding: 0px;">
-  <div class="row no-gutters">
-    <div class="col">
-      <a href="<?= site_url()?>/register">
-        <button class="btn btn-light btn-block" style="height: 60px;">Create a New Account</button>
-      </a>
-    </div>
-  </div>
+<div class="fixed-bottom">
+  <a href="<?= site_url()?>/register">
+    <button class="btn btn-light btn-block" style="height: 60px;">Create a New Account</button>
+  </a> 
 </div>
 
+<?php $this->load->view('partials/footer-script'); ?>
 
-  <?php $this->load->view('partials/footer-script'); ?>
-
-  </body>
+</body>
 </html>
   
-  <?php //$this->load->view('partials/footer'); ?>
+<?php //$this->load->view('partials/footer'); ?>
