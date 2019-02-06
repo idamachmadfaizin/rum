88a71aa32133b61a26f5e3ef98f6f34a419bedd4
idamachmadfaizin <idamachmadfaizin@gmail.com>
2019-02-06 15:01:57 +0700

    login fix

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -11,6 +11,27 @@ class Login extends CI_Controller {
   
   public function index()
   {
+    $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+    $this->form_validation->set_rules('password', 'Password', 'required');
+
+    if ($this->form_validation->run()) {
+      $email = $this->input->post('email');
+      $password = $this->input->post('password');
+      $where = array(
+        'email_customer' => $email,
+        'password_customer' => md5($password)
+      );
+
+      $id = $this->login_model->login($where)->row_array()['id_customer'];
+
+      if ($id) {
+        $_SESSION['id_customer'] = $id;
+        redirect();
+      }else {
+        $this->session->set_flashdata('error_session', 'Email atau password salah!');
+        redirect('/login');
+      }
+    }
     $this->load->view('login');
   }
 
--- application/models/login_model.php
+++ application/models/login_model.php
@@ -3,5 +3,15 @@
 class login_model extends CI_Model 
 {
 
+  public function login($where)
+  {
+    // user this //
+    // $this->db->select('id_customer');
+    // $this->db->where($where);
+    // return $this->db->get('customer')->row_array();
 
+    // or this //
+
+    return $this->db->get_where('customer', $where);
+  }
 }
