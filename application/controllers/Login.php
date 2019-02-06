df1ebd0d26ae5b18a8513b86c821931996c37102
idamachmadfaizin <idamachmadfaizin@gmail.com>
2019-02-06 15:18:08 +0700

    add trim in set_rules

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -11,8 +11,8 @@ class Login extends CI_Controller {
   
   public function index()
   {
-    $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-    $this->form_validation->set_rules('password', 'Password', 'required');
+    $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+    $this->form_validation->set_rules('password', 'Password', 'trim|required');
 
     if ($this->form_validation->run()) {
       $email = $this->input->post('email');
