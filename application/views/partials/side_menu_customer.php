b5712edd75dec38bcec21b83451f96542c04d855
idamachmadfaizin <idamachmadfaizin@gmail.com>
2019-02-09 23:14:31 +0700

    partialed side menu customer

---
+++ application/views/partials/side_menu_customer.php
@@ -0,0 +1,37 @@
+<div class="col-3" style="margin-top:30px;">
+  <ul class="nav flex-column">
+    <li class="nav-item">
+      <a href="" class="nav-link text-decoration-none text-black ml-4 font-weight-bold ">My Account</a>
+    </li>
+    <li class="nav-item side-nav side-nav">
+      <a href="" class="nav-link text-decoration-none text-secondary ml-4">Profile</a>
+    </li>
+    <li class="nav-item side-nav">
+      <a href="" class="nav-link text-decoration-none text-secondary ml-4">Bank</a>
+    </li>
+    <li class="nav-item side-nav">
+      <a href="" class="nav-link text-decoration-none text-secondary ml-4">Address</a>
+    </li>
+    <li class="nav-item side-nav">
+      <a href="" class="nav-link text-decoration-none text-secondary ml-4">Change Password</a>
+    </li>
+  </ul>
+
+  <ul class="nav flex-column mt-4">
+    <li class="nav-item">
+      <a href="" class="nav-link text-decoration-none text-black ml-4 font-weight-bold">My Shopping</a>
+    </li>
+    <li class="nav-item side-nav">
+      <a href="" class="nav-link text-decoration-none text-secondary ml-4">Order</a>
+    </li>
+    <li class="nav-item side-nav">
+      <a href="" class="nav-link text-decoration-none text-secondary ml-4">Puschase</a>
+    </li>
+    <li class="nav-item side-nav">
+      <a href="" class="nav-link text-decoration-none text-secondary ml-4">Delivery of Product</a>
+    </li>
+    <li class="nav-item side-nav">
+      <a href="" class="nav-link text-decoration-none text-secondary ml-4">Report</a>
+    </li>
+  </ul>
+</div>
