560b280cf55345d08dda13c006a19e79f2c4d875
idamachmadfaizin <idamachmadfaizin@gmail.com>
2019-03-03 17:56:22 +0700

    partialsed header and footer admin

--- application/views/partials/footer_admin.php
+++ application/views/partials/footer_admin.php
@@ -1,7 +1,31 @@
-<!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="<?php echo base_url()?>assets/bootstrap/js/jquery-3.3.1.slim.js"></script>
-    <script src="<?php echo base_url()?>assets/bootstrap/js/popper.js"></script>
-    <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.js"></script>
-  </body>
+
+	</div>
+	<!-- /#right-panel -->
+
+	<!-- Scripts -->
+	<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+	<script src="<?= base_url()?>assets/elaadmin/js/main.js"></script>
+
+	<!--  Chart js -->
+	<script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.3/dist/Chart.bundle.min.js"></script>
+
+	<!--Chartist Chart-->
+	<script src="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/chartist-plugin-legend@0.6.2/chartist-plugin-legend.min.js"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/jquery.flot@0.8.3/jquery.flot.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/flot-pie@1.0.0/src/jquery.flot.pie.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/flot-spline@0.0.1/js/jquery.flot.spline.min.js"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/simpleweather@3.1.0/jquery.simpleWeather.min.js"></script>
+	<script src="<?= base_url()?>assets/elaadmin/js/init/weather-init.js"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/moment@2.22.2/moment.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.js"></script>
+	<script src="<?= base_url()?>assets/elaadmin/js/init/fullcalendar-init.js"></script>
+</body>
+
 </html>
--- application/views/partials/header_admin.php
+++ application/views/partials/header_admin.php
@@ -1,43 +1,163 @@
 <!doctype html>
-<html lang="en">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Ela Admin - HTML5 Admin Template</title>
+	<meta name="description" content="Ela Admin - HTML5 Admin Template">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/bootstrap/css/bootstrap.css">
+	<link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
+	<link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
 
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+	<link rel="stylesheet" href="<?= base_url()?>assets/elaadmin/css/cs-skin-elastic.css">
+	<link rel="stylesheet" href="<?= base_url()?>assets/elaadmin/css/style.css">
+	<!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+	<link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
 
-  <title>Admin</title>
+	<link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
+	<link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+	<style>
+		#weatherWidget .currentDesc {
+        color: #ffffff!important;
+    }
+        .traffic-chart {
+            min-height: 335px;
+        }
+        #flotPie1  {
+            height: 150px;
+        }
+        #flotPie1 td {
+            padding:3px;
+        }
+        #flotPie1 table {
+            top: 20px!important;
+            right: -10px!important;
+        }
+        .chart-container {
+            display: table;
+            min-width: 270px ;
+            text-align: left;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        #flotLine5  {
+             height: 105px;
+        }
+
+        #flotBarChart {
+            height: 150px;
+        }
+        #cellPaiChart{
+            height: 160px;
+        }
+
+    </style>
 </head>
-<body class="bg-gray">
 
-<nav class="navbar navbar-light bg-white">
-  <span class="navbar-brand mb-0 h1 ml-5"><b>RUM SEAFOOD</b></span>
-</nav>
+<body style="background-color:#F1F2F7;">
+	<!-- Left Panel -->
+	<aside id="left-panel" class="left-panel">
+		<nav class="navbar navbar-expand-sm navbar-default">
+			<div id="main-menu" class="main-menu collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active">
+						<a href="index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+					</li>
+					<li class="menu-title">Data Master</li><!-- /.menu-title -->
+					<li><a href="<?= site_url().'/admin/category' ?>" class=""><i class="menu-icon fas fa-tags"></i>Category</a></li>
+					<li><a href="#" class=""><i class="menu-icon fa fa-cogs"></i>Product</a></li>
+					<li><a href="#" class=""><i class="menu-icon fa fa-cogs"></i>Customer</a></li>
+					
+          <li class="menu-title">Data Master</li><!-- /.menu-title -->
+					<li><a href="#" class=""><i class="menu-icon fa fa-cogs"></i>Order</a></li>
+					<li><a href="#" class=""><i class="menu-icon fa fa-cogs"></i>Purchase</a></li>
+					<li><a href="#" class=""><i class="menu-icon fa fa-cogs"></i>Delivery of Product</a></li>
+					<li><a href="#" class=""><i class="menu-icon fa fa-cogs"></i>Report</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</nav>
+	</aside>
+	<!-- /#left-panel -->
+	<!-- Right Panel -->
+	<div id="right-panel" class="right-panel">
+		<!-- Header-->
+		<header id="header" class="header">
+			<div class="top-left">
+				<div class="navbar-header">
+					<span class="navbar-brand" style="font-family:'Montserrat'"><b>RUM SEAFOOD</b></span>
+					<!-- <a class="navbar-brand hidden" href="./"><img src="<?php // base_url()?>assets/elaadmin/images/logo2.png" alt="Logo"></a> -->
+					<a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
+				</div>
+			</div>
+			<div class="top-right">
+				<div class="header-menu">
+					<div class="header-left">
+						<button class="search-trigger"><i class="fa fa-search"></i></button>
+						<div class="form-inline">
+							<form class="search-form">
+								<input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
+								<button class="search-close" type="submit"><i class="fa fa-close"></i></button>
+							</form>
+						</div>
 
-<div class="container-fluid">
-  <div class="row flex-xl-nowrap">
-    <div class="col-12 col-md-3 col-xl-3 bd-sidebar">
-    
-      <!-- <nav class="nav flex-column bg-white">
-        <a class="nav-link active" href="#">Active</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </nav> -->
+						<!-- NOTIFICATION -->
+						<!-- <div class="dropdown for-notification">
+							<button class="btn btn-secondary dropdown-toggle" type="button" id="notification" data-toggle="dropdown"
+							 aria-haspopup="true" aria-expanded="false">
+								<i class="fa fa-bell"></i>
+								<span class="count bg-danger">3</span>
+							</button>
+							<div class="dropdown-menu" aria-labelledby="notification">
+								<p class="red">You have 3 Notification</p>
+								<a class="dropdown-item media" href="#">
+									<i class="fa fa-check"></i>
+									<p>Server #1 overloaded.</p>
+								</a>
+								<a class="dropdown-item media" href="#">
+									<i class="fa fa-info"></i>
+									<p>Server #2 overloaded.</p>
+								</a>
+								<a class="dropdown-item media" href="#">
+									<i class="fa fa-warning"></i>
+									<p>Server #3 overloaded.</p>
+								</a>
+							</div>
+						</div> -->
 
-    </div>
-    <div class="col-9">idam</div>
-  </div>
-</div>
+					</div>
 
-<!-- <nav class="collapse bd-links" id="bd-docs-nav">
-  <div class="bd-toc-item">
-    <a class="bd-toc-link" href="/docs/4.3/getting-started/introduction/">
-      Getting started
-    </a>
-  </div>
-</div> -->
+					<div class="user-area dropdown float-right">
+						<a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img class="user-avatar rounded-circle" src="<?= base_url()?>assets/elaadmin/images/admin.jpg" alt="User Avatar">
+						</a>
+
+						<div class="user-menu dropdown-menu">
+							<a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a>
+
+							<a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
+
+							<a class="nav-link" href="#"><i class="fa fa-power -off"></i>Logout</a>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</header>
+		<!-- /#header -->
